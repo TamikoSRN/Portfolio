44a8807943d4a0ce8fd8ca17fe1b07f283bdc3e7
TamikoSRN <thomasereno1@gmail.com>
2022-04-21 23:54:46 -0300

    update sliders

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
 
-import "./Works.scss"
+import "./Works.scss";
 
 export default function Works() {
+
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title:"Mobile Application",
+      desc:
+      "Lorem Ipsum blablablablablablablablablablablabla.",
+      img: "../../../public/assets/nippyApp.jpg"
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title:"Mobile Application",
+      desc:
+      "Lorem Ipsum blablablablablablablablablablablabla.",
+      img: "../../../public/assets/nippyApp.jpg"
+    },
+    {
+      id: "1",
+      icon: "./assets/globe.png",
+      title:"Mobile Application",
+      desc:
+      "Lorem Ipsum blablablablablablablablablablablabla.",
+      img: "../../../public/assets/nippyApp.jpg"
+    },
+  ]
   return (
-    <div className='works' id="works"></div>
-  )
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                  exercitationem iste saepe odio fuga corrupti dignissimos
+                  deserunt impedit placeat ullam odit hic.
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src="assets/fateGOApp.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" />
+      <img src="assets/arrow.png" className="arrow right" alt="" />
+    </div>
+  );
 }
