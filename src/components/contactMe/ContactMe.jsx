0c7e8cf794883ba2715868dc2893732f5bd45394
TamikoSRN <thomasereno1@gmail.com>
2022-04-22 03:40:25 -0300

    contact page finished, started responsive adaptation

--- src/components/contactMe/ContactMe.jsx
+++ src/components/contactMe/ContactMe.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import "./ContactMe.scss"
+import "./ContactMe.scss";
 
 export default function ContactMe() {
+  
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
-    <div className='contactMe' id="contactMe"></div>
-  )
+    <div className="contactMe" id="contactMe">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="E-mail" />
+          <textarea placeholder="Mesasage"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thank you for contacting me, I will reply ASAP! ^^</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
